800def0cc1e4d2fe879f72757c9f4ef71b411498
princeyadav283203@gmail.com <princeyadav283203@gmail.com>
2022-03-27 12:15:22 +0530

    design complete

--- 11_Browser/Project-1_Jira_Ticket_clone/script.js
+++ 11_Browser/Project-1_Jira_Ticket_clone/script.js
@@ -1,4 +1,16 @@
 let addbtn=document.querySelector('.add-btn')
-addbtn.addEventListener('click',function(){
-    console.log("linked")
+let modalcont=document.querySelector(".modal-cont")
+let addflag=false
+
+addbtn.addEventListener('click',function(e){
+    //Display the modal
+    //addflag==true -> Modal Display
+    //addflag==false -> Modal Hide
+    addflag=!addflag
+
+    if(addflag){
+        modalcont.style.display='flex'
+    }else{
+        modalcont.style.display='none'
+    }
 })

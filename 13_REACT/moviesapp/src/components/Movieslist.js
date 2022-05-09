d55c840398c87dda880f3f66fa7cf16618eda79c
princeyadav283203@gmail.com <princeyadav283203@gmail.com>
2022-05-09 17:08:09 +0530

    api linked

--- 13_REACT/moviesapp/src/components/Movieslist.js
+++ 13_REACT/moviesapp/src/components/Movieslist.js
@@ -1,18 +1,72 @@
 import React, { Component } from 'react'
 
-import { movies } from '../Moviesdata'
+// import { movies } from '../Moviesdata'
+import axios from 'axios'
 export class Movieslist extends Component {
   constructor() {
+    console.log('constructor')
     super()
     this.state = {
       hover: " ",
-      pArr : [1]
+      pArr: [1],
+      movies:[],
+      currpage :1
     }
   }
-  render() {
+  async componentDidMount() {
+    const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=87278017aef2c8a20f252457882adb59&language=en-US&page=1`)
+    let moviesData = res.data
+    console.log(moviesData)
 
-    let moviesArr = movies.results
-    console.log(moviesArr)
+    this.setState({
+      movies:[...moviesData.results]
+    })
+    console.log('mounting done by cdn link')
+  }
+  changeMovies= async()=>{
+    const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=87278017aef2c8a20f252457882adb59&language=en-US&page=${this.state.currpage}`)
+    let moviesData = res.data
+    console.log(moviesData)
+
+    this.setState({
+      movies:[...moviesData.results]
+    })
+  }
+
+  habdlePrevious=()=>{
+    if(this.state.currpage!=1){
+      this.setState({
+        currpage: this.state.currpage-1
+      }, this.changeMovies)
+    }
+  }
+
+  handlePageClick=(value)=>{
+    if(value!=this.state.currpage){
+      this.setState({
+        currpage:value
+      },this.changeMovies)
+    }
+  }
+
+  handleNext=()=>{
+    let temparr=[]
+
+    for(let i=1;i<=this.state.pArr.length+1;i++){
+      temparr.push(i)
+    }
+
+    this.setState({
+      pArr:[...temparr],
+      currpage:this.state.currpage + 1
+
+    },this.changeMovies)
+    
+  }
+  render() {
+    console.log('render')
+    // let moviesArr = movies.results
+    // console.log(moviesArr)
     return (
       <React.Fragment>
         <div>
@@ -21,7 +75,7 @@ export class Movieslist extends Component {
         </div>
         <div className='movies-list'>
           {
-            moviesArr.map((moviesElem) => (
+            this.state.movies.map((moviesElem) => (
               <div className="card movie-card" onMouseEnter={() => this.setState({ hover: moviesElem.id })} onMouseLeave={() => this.setState({ hover: " " })}>
                 <img src={`https://image.tmdb.org/t/p/original${moviesElem.backdrop_path}`} style={{ height: '40vh', width: '20vw' }} className="card-img-top movie-img" alt="..." />
 
@@ -48,16 +102,16 @@ export class Movieslist extends Component {
         </div>
         <div style={{ display: 'flex', justifyContent: 'center' }}>
           <nav aria-label="Page navigation example">
-            <ul class="pagination">
-              <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+            <ul className="pagination">
+              <li className="page-item"><a class="page-link" onClick={this.habdlePrevious}>Previous</a></li>
               {
-                this.state.pArr.map((value)=>(
-                  <li class="page-item"><a class="page-link" href="#">{value}</a></li>
+                this.state.pArr.map((value) => (
+                  <li className="page-item"><a class="page-link" onClick={()=>this.handlePageClick(value)}>{value}</a></li>
                 ))
               }
-              
-            
-              <li class="page-item"><a class="page-link" href="#">Next</a></li>
+
+
+              <li className="page-item"><a class="page-link" onClick={this.handleNext}>Next</a></li>
             </ul>
           </nav>
         </div>
